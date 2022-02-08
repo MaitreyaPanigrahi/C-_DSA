4e8e61120cf6b8b14f2f90e4ce8a8e8b71e1f85b
Maitreya <l.29.maitreya@gmail.com>
2022-02-08 19:36:16 +0530

    ninth commit

---
+++ 24_Stack_MinimumCostToMakeAStringValid.cpp
@@ -0,0 +1,40 @@
+  
+//     Odd PArt
+    if(str.length() %2 == 1){
+        return -1;
+    }
+    
+    stack<char>s;
+    for(int i=0;i<str.length();i++){
+        char ch = str[i];
+        
+        if(ch == '{'){
+            s.push(ch);
+        }
+        else{
+//             ch is a closed brace
+            if(!s.empty() && s.top() == '{'){
+                s.pop();
+            }
+            else{
+                s.push(ch);
+            }
+        }
+    }
+    
+//         Stack contain invalid expresions
+        
+        int a  = 0, b = 0;
+        
+        
+        while(!s.empty()){
+            if(s.top() == '{'){
+                b++;
+            }
+            else{
+                a++;
+            }
+            s.pop();
+        }
+        int ans = (a+1)/2 + (b+1)/2;
+        return ans;
